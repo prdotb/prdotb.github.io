598382cab501fcec436118a88a7a9b351323f0da
craigmelbourne <craig.melbourne@gmail.com>
2025-08-06 16:16:24 +0100

    anywhere search dest filter

---
+++ anywheresearch/hotels.js
@@ -0,0 +1,524 @@
+{
+	"hotels": [
+		{
+			"name" :"BLUESEA Club Marthas", 
+			"starRating": 4,
+			"ta" : {
+				"taRating": 4.2, 
+				"taReviewsNum": 2023
+			}, 
+			"destination" : {
+				"resort": "Cala D Or,", 
+				"region" : "Majorca,",
+				"country" : "Spain"
+			}, 
+			"flight" : {
+				"departureAirport": "London Gatwick", 
+				"direct": "Direct",
+				"holdBags": 0
+			},
+			"boardBasis" : "All Inclusive", 
+			"dateRange" : "Wed 10 Jun 2026 - Wed 17 Jun 2026", 
+			"price" : {
+				"totalPrice" : "1028", 
+				"pricePerPerson" : "514"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1180,quality=70,fit=cover,format=auto/m/75ca2b4706daf14b/original/BLUESEA-Club-Marthas.jpg" 
+
+		}, 
+		{
+			"name":"Caves Beach Resort - Adults Only (16+)", 
+			"starRating": 4,
+			"ta" : {
+				"taRating": 4.5, 
+				"taReviewsNum": 2774
+			}, 
+			"destination" : {
+				"resort": "Hurghada,", 
+				"region" : "",
+				"country" : "Egypt"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Gatwick", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "All Inclusive", 
+			"dateRange" : "Wed 10 Jun 2026 - Wed 17 Jun 2026", 
+			"price" : {
+				"totalPrice" : "1242", 
+				"pricePerPerson" : "621"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1180,quality=70,fit=cover,format=auto/m/63abba2048b77f14/original/Caves-Beach.jpg" 
+
+		},
+		{
+			"name":"Sol Pelicanos Ocas", 
+			"starRating": 3,
+			"ta" : {
+				"taRating": 4.0, 
+				"taReviewsNum": 11585
+			}, 
+			"destination" : {
+				"resort": "Benidorm,", 
+				"region" : "Costa Blanca,",
+				"country" : "Spain"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Gatwick", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "Bed & Breakfast", 
+			"dateRange" : "Tue 9 Jun 2026 - Tue 16 Jun 2025", 
+			"price" : {
+				"totalPrice" : "954", 
+				"pricePerPerson" : "477"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1400,quality=70,fit=cover,format=auto/m/5d4b485c8c17eb7/original/Sol-Pelicanos-Ocas.jpg" 
+
+		},
+		{
+			"name":"Sol Tenerife", 
+			"starRating": 4,
+			"ta" : {
+				"taRating": 4.5, 
+				"taReviewsNum": 6467
+			}, 
+			"destination" : {
+				"resort": "Playa De Las Americas,", 
+				"region" : "Tenerife,",
+				"country" : "Spain"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Gatwick", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "Bed & Breakfast", 
+			"dateRange" : "Tue 2 Jun 2026 - Tue 9 Jun 2026", 
+			"price" : {
+				"totalPrice" : "918", 
+				"pricePerPerson" : "459"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1400,quality=70,fit=cover,format=auto/m/145899c0137b6e98/original/Sol-Tenerife.jpg" 
+
+		}, 
+		{
+			"name":"The Ixian Grand & All Suites - Adults Only (18+)", 
+			"starRating": 5,
+			"ta" : {
+				"taRating": 4.0, 
+				"taReviewsNum": 3969
+			}, 
+			"destination" : {
+				"resort": "Ixia,", 
+				"region" : "Rhodes,",
+				"country" : "Greece"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Gatwick", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "All Inclusive", 
+			"dateRange" : "Tue 16 Jun 2026 - Wed 24 Jun 2026", 
+			"price" : {
+				"totalPrice" : "1,814", 
+				"pricePerPerson" : "907"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1400,quality=70,fit=cover,format=auto/m/237b2c3814fb24f3/original/Ixian-Grand-and-All-Suites-New-8.jpg" 
+
+		},
+		{
+			"name":"BLUESEA Costa Teguise Gardens", 
+			"starRating": 3,
+			"ta" : {
+				"taRating": 3.0, 
+				"taReviewsNum": 3656
+			}, 
+			"destination" : {
+				"resort": "Costa Teguise,", 
+				"region" : " Lanzarote,",
+				"country" : "Spain"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Luton", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "Bed & Breakfast", 
+			"dateRange" : "Tue 2 Jun 2026 - Tue 9 Jun 2026", 
+			"price" : {
+				"totalPrice" : "814", 
+				"pricePerPerson" : "407"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1400,quality=70,fit=cover,format=auto/m/7d210182220f6d90/original/BLUESEA-Costa-Teguise-Gardens.jpg" 
+
+		},
+		{
+			"name":"Grand Pasa Hotel", 
+			"starRating": 5,
+			"ta" : {
+				"taRating": 4.5, 
+				"taReviewsNum": 4393
+			}, 
+			"destination" : {
+				"resort": "Marmaris,", 
+				"region" : " Dalaman,",
+				"country" : "Turkey"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Luton", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "All Inclusive", 
+			"dateRange" : "Fri 5 Jun 2026 - Fri 12 Jun 2026", 
+			"price" : {
+				"totalPrice" : "1,114", 
+				"pricePerPerson" : "557"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1400,quality=70,fit=cover,format=auto/m/471bf7bb1db26e54/original/Grand-Pasa-Hotel.jpg" 
+
+		}, 
+		{
+			"name":"X Life Hotel - Adults Only (15+)", 
+			"starRating": 4,
+			"ta" : {
+				"taRating": 3.5, 
+				"taReviewsNum": 363
+			}, 
+			"destination" : {
+				"resort": "Sarigerme,", 
+				"region" : " Dalaman,",
+				"country" : "Turkey"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Gatwick", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "All Inclusive", 
+			"dateRange" : "Thu 11 Jun 2026 - Thu 18 Jun 2026", 
+			"price" : {
+				"totalPrice" : "1,275", 
+				"pricePerPerson" : "638"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1400,quality=70,fit=cover,format=auto/m/286b2d2c6df8b2f5/original/XL-Sarigerme-ex-Castle-Resort-Adults-Only.jpg" 
+
+		},
+		{
+			"name":"BLUESEA Montevista Hawaii", 
+			"starRating": 3,
+			"ta" : {
+				"taRating": 3, 
+				"taReviewsNum": 294
+			}, 
+			"destination" : {
+				"resort": "Lloret De Mar,", 
+				"region" : "Costa Brava,",
+				"country" : "Spain"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Luton", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "Bed & Breakfast", 
+			"dateRange" : "Tue 2 Jun 2026 - Tue 9 Jun 2026", 
+			"price" : {
+				"totalPrice" : "552", 
+				"pricePerPerson" : "276"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1400,quality=70,fit=cover,format=auto/m/1441644848e0fbb8/original/Blue-Sea-Montevista-Hawai.jpg" 
+
+		},
+		{
+			"name":"Hotel Angela - Adults-Only", 
+			"starRating": 4,
+			"ta" : {
+				"taRating": 4, 
+				"taReviewsNum": 2338
+			}, 
+			"destination" : {
+				"resort": "Fuengirola,", 
+				"region" : "Costa del Sol,",
+				"country" : "Spain"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Gatwick", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "Bed & Breakfast", 
+			"dateRange" : "Wed 10 Jun 2026 - Wed 17 Jun 2026", 
+			"price" : {
+				"totalPrice" : "1,062", 
+				"pricePerPerson" : "531"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1400,quality=70,fit=cover,format=auto/m/49b3e4a4deab497f/original/Hotel-Angela.jpg" 
+
+		},
+		{
+			"name":"Ibiza Rocks Hotel - Adults Only", 
+			"starRating": 3,
+			"ta" : {
+				"taRating": 4, 
+				"taReviewsNum": 2338
+			}, 
+			"destination" : {
+				"resort": "San Antonio,", 
+				"region" : "Ibiza,",
+				"country" : "Spain"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Gatwick", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "Room Only", 
+			"dateRange" : "Tue 9 Jun 2026 - Tue 16 Jun 2026", 
+			"price" : {
+				"totalPrice" : "1,390", 
+				"pricePerPerson" : "695"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1400,quality=70,fit=cover,format=auto/m/3144103b3713a0a1/original/Ibiza-Rocks-Hotel-Adults-Only.jpg" 
+
+		},
+		{
+			"name":"Port River Hotel & Spa", 
+			"starRating": 5,
+			"ta" : {
+				"taRating": 4, 
+				"taReviewsNum": 2304
+			}, 
+			"destination" : {
+				"resort": "Side,", 
+				"region" : "Antalya,",
+				"country" : " Turkey"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Gatwick", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "All Inclusive", 
+			"dateRange" : "Sat 20 Jun 2026 - Sat 27 Jun 2026", 
+			"price" : {
+				"totalPrice" : "1,188", 
+				"pricePerPerson" : "594"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1400,quality=70,fit=cover,format=auto/m/5d4c14c2a6094072/original/Port-River-Hotel-Spa.jpg" 
+
+		},
+		{
+			"name":"Hotel Gran Cervantes by BLUESEA", 
+			"starRating": 4,
+			"ta" : {
+				"taRating": 3.5, 
+				"taReviewsNum": 2304
+			}, 
+			"destination" : {
+				"resort": "Torremolinos,", 
+				"region" : "Costa del Sol,",
+				"country" : "Spain"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Gatwick", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "All Inclusive", 
+			"dateRange" : "Wed 3 Jun 2026 - Wed 10 Jun 2026", 
+			"price" : {
+				"totalPrice" : "992", 
+				"pricePerPerson" : "496"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1400,quality=70,fit=cover,format=auto/m/7246e05799c19546/original/Hotel-Gran-Cervantes-by-Blue-Sea.jpg" 
+
+		},
+		{
+			"name":"Stella Island Luxury Resort & Spa - Adults Only", 
+			"starRating": 5,
+			"ta" : {
+				"taRating": 4.5, 
+				"taReviewsNum": 1794
+			}, 
+			"destination" : {
+				"resort": "Analipsis,", 
+				"region" : "Crete,",
+				"country" : "Greece"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Gatwick", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "All Inclusive", 
+			"dateRange" : "Tue 16 Jun 2026 - Tue 23 Jun 2026", 
+			"price" : {
+				"totalPrice" : "2,683", 
+				"pricePerPerson" : "1,342"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1400,quality=70,fit=cover,format=auto/m/70e76cd80fe4d3ef/original/Stella-Island-Luxury-Resort-Spa-Adults-Only.jpg" 
+
+		},
+		{
+			"name":"Voxx Marmaris Beach Resort (Adults Only 16+)", 
+			"starRating": 4,
+			"ta" : {
+				"taRating": 4, 
+				"taReviewsNum": 1794
+			}, 
+			"destination" : {
+				"resort": "Hisaronu (Marmaris),", 
+				"region" : "Dalaman,",
+				"country" : "Turkey"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Gatwick", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "All Inclusive", 
+			"dateRange" : "Thu 11 Jun 2026 - Thu 18 Jun 2026", 
+			"price" : {
+				"totalPrice" : "932", 
+				"pricePerPerson" : "466"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1400,quality=70,fit=cover,format=auto/m/266bbc278cae3c65/original/Voxx-Marmaris-Beach-Resort.jpg" 
+
+		},
+		{
+			"name":"Sol House The Studio - Adults Only (16+)", 
+			"starRating": 4,
+			"ta" : {
+				"taRating": 4, 
+				"taReviewsNum": 1087
+			}, 
+			"destination" : {
+				"resort": "Magaluf,", 
+				"region" : "Majorca,",
+				"country" : "Spain"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Stansted", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "All Inclusive", 
+			"dateRange" : "Tue 2 Jun 2026 - Tue 9 Jun 2026", 
+			"price" : {
+				"totalPrice" : "890", 
+				"pricePerPerson" : "445"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1400,quality=70,fit=cover,format=auto/m/2d707096904bd31e/original/Sol-House-The-Studio.jpg" 
+
+		},
+		{
+			"name":"Barcelo Teguise Beach - Adults Only", 
+			"starRating": 4,
+			"ta" : {
+				"taRating": 4.5, 
+				"taReviewsNum": 9348
+			}, 
+			"destination" : {
+				"resort": "Costa Teguise,", 
+				"region" : "Lanzarote,",
+				"country" : "Spain"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Luton", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "All Inclusive", 
+			"dateRange" : "Tue 2 Jun 2026 - Tue 9 Jun 2026", 
+			"price" : {
+				"totalPrice" : "1,503", 
+				"pricePerPerson" : "752"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1400,quality=70,fit=cover,format=auto/m/727fe277152e81e9/original/Barcelo-Teguise-Beach-Adults-Only.jpg" 
+
+		},
+		{
+			"name":"Laguna Park II", 
+			"starRating": 2,
+			"ta" : {
+				"taRating": 3, 
+				"taReviewsNum": 4470
+			}, 
+			"destination" : {
+				"resort": "Costa Adeje,", 
+				"region" : "Tenerife,",
+				"country" : "Spain"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Luton", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "Self Catering", 
+			"dateRange" : "Tue 2 Jun 2026 - Tue 9 Jun 2026", 
+			"price" : {
+				"totalPrice" : "769", 
+				"pricePerPerson" : "385"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1180,quality=70,fit=cover,format=auto/m/55bf6202e67b37b4/original/Laguna-Park-II.jpg" 
+
+		},
+		{
+			"name":"Sol Palmanova", 
+			"starRating": 4,
+			"ta" : {
+				"taRating": 3.5, 
+				"taReviewsNum": 4470
+			}, 
+			"destination" : {
+				"resort": "Palma Nova,", 
+				"region" : "Majorca,",
+				"country" : "Spain"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Stansted", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "All Inclusive", 
+			"dateRange" : "Tue 2 Jun 2026 - Tue 9 Jun 2026", 
+			"price" : {
+				"totalPrice" : "1,129", 
+				"pricePerPerson" : "565"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1180,quality=70,fit=cover,format=auto/m/36e28c6327c07daa/original/Sol-Palmanova.jpg" 
+
+		},
+		{
+			"name":"Stella Blue Seaside Resort - Adults Only (12+)", 
+			"starRating": 4,
+			"ta" : {
+				"taRating": 4.5, 
+				"taReviewsNum": 57
+			}, 
+			"destination" : {
+				"resort": "Analipsis,", 
+				"region" : "Crete,",
+				"country" : "Greece"
+			}, 
+			"flight" : {
+				"departureAirport" : "London Gatwick", 
+				"direct" : "Direct",
+				"holdBags" : 0
+			},
+			"boardBasis" : "All Inclusive", 
+			"dateRange" : "Thu 11 Jun 2026 - Thu 18 Jun 2026", 
+			"price" : {
+				"totalPrice" : "2,079", 
+				"pricePerPerson" : "1,040"
+			}, 
+			"images" : "https://bynder.onthebeach.co.uk/cdn-cgi/image/width=1180,quality=70,fit=cover,format=auto/m/462f4603a1373574/original/Stella-Blue-Seaside-Resort.jpg" 
+
+		}
+	]
+}
